7246605ef52b8497cf7c7b2b9d33536ade2bfb61
Rack Lin <racklin@gmail.com>
2013-08-08 02:01:55 +0800

    Added lang: 'a' - 國語 , lang: 't' - 閩南語 支援
    Signed-off-by: Rack Lin <racklin@gmail.com>

--- jquery.moedict.js
+++ jquery.moedict.js
@@ -8,22 +8,30 @@
   (function($){
     var moedictConfig;
     moedictConfig = {
-      initialized: false,
-      regex: null
+      initialized_a: false,
+      regex_a: null,
+      initialized_t: false,
+      regex_t: null
     };
     window._moedictDataCallback = function(data){
-      var keys, words;
+      var lang, lenToRegex, keys, words;
+      lang = 'a';
       if (data.lenToRegex != null) {
-        keys = Object.keys(data.lenToRegex).sort(function(a, b){
-          return b - a;
-        });
-        words = [];
-        keys.forEach(function(k){
-          return words.push(data.lenToRegex[k]);
-        });
-        moedictConfig.regex = new RegExp(words.join('|'), 'g');
-        return moedictConfig.initialized = true;
+        lang = 'a';
+        lenToRegex = data.lenToRegex;
+      } else if (data.lenToRegex_t != null) {
+        lang = 't';
+        lenToRegex = data.lenToRegex_t;
       }
+      keys = Object.keys(lenToRegex).sort(function(a, b){
+        return b - a;
+      });
+      words = [];
+      keys.forEach(function(k){
+        return words.push(lenToRegex[k]);
+      });
+      moedictConfig["regex_" + lang] = new RegExp(words.join('|'), 'g');
+      return moedictConfig["initialized_" + lang] = true;
     };
     /**
      * moedict plugin function
@@ -35,12 +43,14 @@
      *               analyze: true - return words analyzation
      *               dryrun: true - return replaced html
      *               callback: function - receive analyze or dryrun data
+     *               lang:[a|t] - a 國 / t 閩南
      *
      * $('#content').moedict();
      */
     $.fn.moedict = function(config){
       var replaceContent, initRegexp, this$ = this;
       config = $.extend({
+        lang: 'a',
         'class': 'moedict-word',
         spaced: false,
         draw: false,
@@ -48,14 +58,21 @@
         dryrun: false,
         callback: null
       }, config);
+      if (['a', 't'].indexOf(config.lang) === -1) {
+        config.lang = 'a';
+      }
       replaceContent = function(){
         this$.each(function(){
-          var $elem, text, wordUsage, html, newHtml;
+          var $elem, regex, ref$, langPrefix, text, wordUsage, html, newHtml;
           $elem = $(this);
+          regex = (ref$ = moedictConfig["regex_" + config.lang]) != null
+            ? ref$
+            : moedictConfig["regex_a"];
+          langPrefix = config.lang === 't' ? '!' : '';
           if (config.analyze) {
             text = $elem.text();
             wordUsage = {};
-            text.replace(moedictConfig.regex, function(w){
+            text.replace(regex, function(w){
               if (wordUsage[w]) {
                 return wordUsage[w]++;
               } else {
@@ -67,12 +84,12 @@
             }
           } else {
             html = $elem.html();
-            newHtml = html.replace(moedictConfig.regex, function(w){
+            newHtml = html.replace(regex, function(w){
               var buf;
               if (config.draw) {
-                buf = "<a href='https://www.moedict.tw/?draw#" + w + "' class='" + config['class'] + "'>" + w + "</a>";
+                buf = "<a href='https://www.moedict.tw/?draw#" + langPrefix + w + "' class='" + config['class'] + "'>" + w + "</a>";
               } else {
-                buf = "<a href='https://www.moedict.tw/#" + w + "' class='" + config['class'] + "'>" + w + "</a>";
+                buf = "<a href='https://www.moedict.tw/#" + langPrefix + w + "' class='" + config['class'] + "'>" + w + "</a>";
               }
               if (config.spaced) {
                 buf += " ";
@@ -101,22 +118,21 @@
         return this$;
       };
       initRegexp = function(cb){
-        $.ajax({
+        return $.ajax({
           type: 'GET',
           dataType: 'jsonp',
           jsonp: false,
           cache: true,
           jsonCallback: '_moedictDataCallback',
-          url: 'https://www.moedict.tw/lenToRegex.a.json.js',
+          url: "https://www.moedict.tw/lenToRegex." + config.lang + ".json.js",
           complete: function(){
             if (cb) {
               return cb.call();
             }
           }
         });
-        return moedictConfig.initialized;
       };
-      if (!moedictConfig.initialized) {
+      if (!moedictConfig["initialized_" + config.lang]) {
         return initRegexp(replaceContent);
       } else {
         return replaceContent();
